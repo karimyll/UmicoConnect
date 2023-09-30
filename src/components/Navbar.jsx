fc17fda8713b18bf2854f73b0084380992a3484e
thekrmff <karimoff676@gmail.com>
2023-09-30 10:54:32 +0400

    sa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import HomeIcon from '../assets/img/home.svg'
-import 
+import RankingIcon from '../assets/img/ranking.svg'
+import MyOrdersIcon from '../assets/img/myorders.svg'
+import ProfileIcon from '../assets/img/profile.svg'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
@@ -8,7 +10,21 @@ const Navbar = () => {
     <div className="navbar">
       <NavLink to='/home' activeClassName='active'>
         <img src={HomeIcon} alt="" />
+        <p>Home</p>
       </NavLink>
+      <NavLink to='/ranking' activeClassName='active'>
+        <img src={RankingIcon} alt="" />
+        <p>Ranking</p>
+      </NavLink>
+      <NavLink to='/myorders' activeClassName='active'>
+        <img src={MyOrdersIcon} alt="" />
+        <p>My Orders</p>
+      </NavLink>
+      <NavLink to='/profile' activeClassName='active'>
+        <img src={ProfileIcon} alt="" />
+        <p>Profile</p>
+      </NavLink>
+
     </div>
   )
 }
